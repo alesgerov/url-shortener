9e00015072ee05f1a245f41a27b8403e398bf2b5
alesgerov <alesgerov@alesgerov.com>
2023-11-13 20:49:28 +0400

    - small fix

--- src/main/java/com/alesgerov/urlShortener/configuration/RedisConfiguration.java
+++ src/main/java/com/alesgerov/urlShortener/configuration/RedisConfiguration.java
@@ -2,13 +2,14 @@ package com.alesgerov.urlShortener.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Alasgarov Tofig
  * @date 11/7/2023
  */
-@Component
+@Configuration
 @ConfigurationProperties("application.redis")
 @Data
 public class RedisConfiguration {
