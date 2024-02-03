39bd54f2998653a719b84795b4f20800cccf8f35
alesgerov <alesgerov@alesgerov.com>
2024-02-04 01:23:11 +0400

    reformatted

--- src/main/java/com/alesgerov/urlShortener/configuration/RedisConfiguration.java
+++ src/main/java/com/alesgerov/urlShortener/configuration/RedisConfiguration.java
@@ -3,7 +3,6 @@ package com.alesgerov.urlShortener.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @author Alasgarov Tofig
